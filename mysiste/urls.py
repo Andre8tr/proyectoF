3937a7edd483d337f6e41d7c1a6f6311affaadb0
Andre Tirado <andre8tr@gmail.com>
2016-11-21 22:49:36 -0600

    Ya hice el Login pero no me aparece la pagina, solo eso falta

--- mysiste/urls.py
+++ mysiste/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.contrib.auth.views import login
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysiste.views.home', name='home'),
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('ligas.urls')),
     url(r'', include('usuarios.urls')),
+    url(r'^$', login, {'template_name:index.html'}, name="login")
+
 ]
